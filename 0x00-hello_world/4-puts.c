2262743ccb938c1cd83df0d5670797113a270985
zoz01 <ziadgad2002@gmail.com>
2023-08-10 16:24:19 -0700

    My first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
-
 /** 
 *main - entry for the programm
 *Return: always 0
 */
-
-int main(void){
+int main(void)
+{
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
