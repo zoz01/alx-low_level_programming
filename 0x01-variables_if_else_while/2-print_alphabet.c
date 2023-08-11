e530cf2532ac84b97cac3643ae7d53826d84f780
zoz01 <ziadgad2002@gmail.com>
2023-08-11 15:29:18 -0700

    My first commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+*main - entiry point
+*Return:is always 0
+*/
+int main(void)
+{
+char mych;
+for (mych = 'a'; mychar <= 'z'; mychar++)
+putchar(mychar);
+putchar('\n');
+return (0);
+}
