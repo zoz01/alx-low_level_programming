f51a85dcced970b88fb1566f05021779153ed9c9
zoz01 <ziadgad2002@gmail.com>
2023-08-11 17:14:05 -0700

    My first commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+*main - entiry point
+*Return:is always 0
+*/
+int main(void)
+{
+	char mlml;
+
+	for (mlml = 'z'; mlml >= 'a'; mlml--)
+		putchar(mlml);
+		putchar('\n');
+	return (0);
+}
