e577200917cf7de31fb5deb46abf00a19363095b
zoz01 <ziadgad2002@gmail.com>
2023-08-11 17:08:07 -0700

    My first commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+*main - entiry point
+*Return:is always 0
+*/
+int main(void)
+{
+	int mynumber;
+
+	for (mynumber = 0; mynumber < 10; mynumber++)
+		printf("%d", mynumber);
+	printf("\n");
+	return (0);
+}
