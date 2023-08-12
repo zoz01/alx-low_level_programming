2461ad4f14fcce6e86a387b0bf991336e38e9bbd
zoz01 <ziadgad2002@gmail.com>
2023-08-11 17:11:01 -0700

    My first commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+*main - entiry point
+*Return:is always 0
+*/
+int main(void)
+{
+	int mydf;
+
+	for (mydf = '0'; mydf <= '9'; mydf++)
+	putchar(mydf);
+	putchar('\n');
+	return (0);
+}
