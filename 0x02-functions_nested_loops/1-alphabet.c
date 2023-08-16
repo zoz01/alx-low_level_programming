561ac17234f26953e59e5a1f925e68485dcc7ba5
zoz01 <ziadgad2002@gmail.com>
2023-08-15 17:46:36 -0700

    My first commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*print_alphabet - new alphapet
+*Return: doesnot return
+*/
+void print_alphabet(void)
+{
+int letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+_putchar (letter);
+}
+_putchar (10);
+}
