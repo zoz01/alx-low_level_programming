74c502bbc618470f21612e10cc0a68c3a66f6976
zoz01 <ziadgad2002@gmail.com>
2023-08-15 18:16:21 -0700

    My first commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _abs - the abs of a char
+*@i: index or wanted number
+*Return: will return 0
+*/
+int _abs(int i)
+{
+if (i > 0)
+return (i);
+else if (i < 0)
+return (-i);
+else
+return (0);
+}
