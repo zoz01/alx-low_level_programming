f4f4210325afbbe7966ec710e2df1d2e1ed009f9
zoz01 <ziadgad2002@gmail.com>
2023-08-15 17:26:50 -0700

    My first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*main - intery point
+*Return:always return= 0
+*/
+int main(void)
+{
+char school[10] = "_putchar";
+int i;
+for (i = 0; i < 8; i++)
+{
+_putchar(school[i]);
+}
+_putchar(10);
+return (0);
+}
