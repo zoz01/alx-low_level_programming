d3ae199eab6a70efba169b239618c792c6bca7fe
zoz01 <ziadgad2002@gmail.com>
2023-08-17 19:59:27 -0700

    My first commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*print_numbers - print all numbers 0 to 9
+*Return: will be none
+*/
+void print_numbers(void)
+{
+int number = 48;
+for (number = 48; number < 58; number++)
+{
+_putchar(number);
+}
+_putchar(10);
+}
