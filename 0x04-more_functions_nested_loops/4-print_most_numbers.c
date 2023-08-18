73d0faae40bb1a73b1272f1f71b4c5618665f81e
zoz01 <ziadgad2002@gmail.com>
2023-08-17 20:00:34 -0700

    My first commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*print_most_numbers - prints numbers except 2 , 4
+*Return: nothing
+*/
+void print_most_numbers(void)
+{
+int number;
+for (number = 48; number < 58; number++)
+{
+if ((number == 50) || (number == 52))
+{
+continue;
+}
+_putchar(number);
+}
+_putchar(10);
+}
