818c65ab4cbca426d296e1335ebfb97258f8c395
zoz01 <ziadgad2002@gmail.com>
2023-08-17 20:33:30 -0700

    My first commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*print_line - prints straight line
+*@n: a parameter
+*Return: none is returned
+*/
+void print_line(int n)
+{
+while (n-- > 0)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
