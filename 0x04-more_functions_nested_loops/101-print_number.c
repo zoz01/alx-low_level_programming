b36d20506a334eef54be11dd7f3a3c754cba8945
zoz01 <ziadgad2002@gmail.com>
2023-08-18 12:38:08 -0700

    My first commit

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print_number - number js printed
+* @n: number printed
+*/
+void print_number(int n)
+{
+unsigned int num = n;
+if (n < 0)
+{
+_putchar('-');
+num = -num;
+}
+if (num > 9)
+{
+print_number(num / 10);
+}
+_putchar(num % 10 + '0');
+}
