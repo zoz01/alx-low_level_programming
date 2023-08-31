9aa081413a2924ea05979f4cd4cfa214a845a347
zoz01 <ziadgad2002@gmail.com>
2023-08-31 08:47:34 -0700

    My first commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,17 @@
 #include "holberton.h"
 /**
- *  * _puts_recursion - prints a string, then a new
- *   * line.
- *     * @s: input is a string
- *      */
-
+ *  *  * _puts_recursion - prints a string, then a new
+ *   ** line.
+ *    ** @s: input is a string
+ *     */
 void _puts_recursion(char *s)
 {
-		if (*s == '\0')
-				{
-							_putchar('\n');
-									return;
-										}
-			_putchar(*s);
-				s++;
-					_puts_recursion(s);
-}
+                if (*s == '\0')
+        {
+	                                                        _putchar('\n');
+	                                                                        return;
+	}
+                        _putchar(*s);
+                                s++;
+                                        _puts_recursion(s);
+}}
