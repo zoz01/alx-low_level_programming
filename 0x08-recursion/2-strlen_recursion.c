cfed69b0d271bae1cebba8eab692d0fe581451fa
zoz01 <ziadgad2002@gmail.com>
2023-08-31 22:14:56 -0700

    My first commit

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,10 +1,10 @@
 nclude "holberton.h"
 /**
- * *_strlen_recursion - for String length
- * *@s: the string
- * *Return: success 1
- * *error, -1
- * */
+*_strlen_recursion - for String length
+*@s: the string
+*Return: success 1
+*error, -1
+*/
 int _strlen_recursion(char *s)
 {
 	        if (*s != '\0')
