dc29f2f99b1575bb6bc3b946d6afb6ab4142da13
zoz01 <ziadgad2002@gmail.com>
2023-09-05 14:49:09 -0700

    My first commit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*_strdup - thepoint
+*@str: duplicated str
+*Return: duplicated string gives pointer or NULL
+*/
+char *_strdup(char *str)
+{
+	char *strn = NULL;
+	unsigned int i;
+	int n;
+
+	if (str == NULL)
+		return (NULL);
+	for (n = 0; str[n] != '\0'; n++)
+		;
+	strn = (char *)malloc(n + 1 * sizeof(char));
+	if (strn != NULL)
+	{
+		for (i = 0; str[i] != '\0'; i++)
+			strn[i] = str[i];
+	} else
+	{
+		return (NULL);
+	}
+	strn[i] = '\0';
+	return (strn);
+}                                                                                                                           
