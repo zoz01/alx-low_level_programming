04d07a18822340afd86e5638f58be38b6b8446cc
zoz01 <ziadgad2002@gmail.com>
2023-09-05 18:03:56 -0700

    My first commit

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,91 +1,112 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-/**
- * number_of_words - function to calculate number of words
- * @str: string being passed to check for words
- *
- * Return: number of words
- */
-int number_of_words(char *str)
+int is_delimiter(char c, const char *delimiters)
 {
-	int index, num = 0;
-
-	for (index = 0; str[index] != '\0'; index++)
-	{
-		if (*str == ' ')
-			str++;
-		else
-		{
-			for (; str[index] != ' ' && str[index] != '\0'; index++)
-				str++;
-			num++;
-		}
-	}
-	return (num);
+    while (*delimiters != '\0')
+    {
+        if (c == *delimiters)
+            return 1;
+        delimiters++;
+    }
+    return 0;
 }
 
-/**
- * free_memory - frees the memory
- * @string_array: pointer values being passed for freeing
- * @count: counter
- */
-void free_memory(char **string_array, int count)
+int count_words(const char *str, const char *delimiters)
 {
-	for (; count > 0;)
-		free(string_array[--count]);
-	free(string_array);
+    int count = 0;
+    int in_word = 0;
+
+    while (*str != '\0')
+    {
+        if (is_delimiter(*str, delimiters))
+        {
+            in_word = 0;
+        }
+        else if (!in_word)
+        {
+            in_word = 1;
+            count++;
+        }
+
+        str++;
+    }
+
+    return count;
 }
 
-/**
- * string_split - function that splits string into words
- * @str: string being passed
- * Return: null if string is empty or null or function fails
- */
-char **string_split(char *str)
+char **strtow(const char *str, const char *delimiters)
 {
-	int total_words = 0, index1 = 0, index2 = 0, length = 0;
-	char **words, *found_word;
+    int total_words, word_length, index;
+    char **words, *word_start;
 
-	if (str == 0 || *str == 0)
-		return (NULL);
-	total_words = number_of_words(str);
-	if (total_words == 0)
-		return (NULL);
-	words = malloc((total_words + 1) * sizeof(char *));
-	if (words == 0)
-		return (NULL);
-	for (; *str != '\0' && index1 < total_words;)
-	{
-		if (*str == ' ')
-			str++;
-		else
-		{
-			found_word = str;
-			for (; *str != ' ' && *str != '\0';)
-			{
-				length++;
-				str++;
-			}
-			words[index1] = malloc((length + 1) * sizeof(char));
-			if (words[index1] == 0)
-			{
-				free_memory(words, index1);
-				return (NULL);
-			}
-			while (*found_word != ' ' && *found_word != '\0')
-			{
-				words[index1][index2] = *found_word;
-				found_word++;
-				index2++;
-			}
-			words[index1][index2] = '\0';
-			index1++;
-			index2 = 0;
-			length = 0;
-			str++;
-		}
-	}
-	return (words);
+    if (str == NULL || *str == '\0')
+        return NULL;
+
+    total_words = count_words(str, delimiters);
+    words = malloc((total_words + 1) * sizeof(char *));
+    if (words == NULL)
+        return NULL;
+
+    index = 0;
+    while (*str != '\0')
+    {
+        if (is_delimiter(*str, delimiters))
+        {
+            str++;
+            continue;
+        }
+
+        word_start = (char *)str;
+        word_length = 0;
+
+        while (!is_delimiter(*str, delimiters) && *str != '\0')
+        {
+            str++;
+            word_length++;
+        }
+
+        words[index] = malloc((word_length + 1) * sizeof(char));
+        if (words[index] == NULL)
+        {
+            while (index > 0)
+                free(words[--index]);
+            free(words);
+            return NULL;
+        }
+
+        strncpy(words[index], word_start, word_length);
+        words[index][word_length] = '\0';
+        index++;
+    }
+
+    words[index] = NULL;
+    return words;
+}
+
+int main(void)
+{
+    const char *str = "Hello, world! This is a test string.";
+    const char *delimiters = " ,.!";
+
+    char **words = strtow(str, delimiters);
+    if (words == NULL)
+    {
+        printf("Failed to split the string.\n");
+        return 1;
+    }
+
+    int i = 0;
+    while (words[i] != NULL)
+    {
+        printf("%s\n", words[i]);
+        free(words[i]);
+        i++;
+    }
+
+    free(words);
+
+    return 0;
 }
