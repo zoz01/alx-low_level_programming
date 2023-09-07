22ed61515b626ed61ccdd232ba02ee4128d46a51
zoz01 <ziadgad2002@gmail.com>
2023-09-07 16:38:26 -0700

    My first commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,41 +1,42 @@
+#include "main.h"
+
 /**
-* custom_string_nconcat - Concatenates two strings using at most a specified
-* number of bytes.
-* @s1: The first input is deff string.
-* @s2: The second input is deffff string.
-* @n: The maximum number of bytes from s2 to concatenate to s1.
-* Return: If the function fails - NULL.
-* Otherwise - a pointer to the allocated memory containing the concatenated string.
+*string_nconcat - this fuc def does somthin
+*@s1: 1st str
+*@s2: 2nd str
+*@n: max bytes s2 to s1
+*Return: fail - NULL.
+*Otherwise - pointer to space
 */
 
-char *custom_string_nconcat(char *s1, char *s2, unsigned int n)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *concatenated;
-    unsigned int total_length = n, i;
+	char *concat;
+	unsigned int len = n, index;
 
-    if (s1 == NULL)
-        s1 = "";
+	if (s1 == NULL)
+		s1 = "";
 
-    if (s2 == NULL)
-        s2 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-    for (i = 0; s1[i]; i++)
-        total_length++;
+	for (index = 0; s1[index]; index++)
+		len++;
 
-    concatenated = malloc(sizeof(char) * (total_length + 1));
+	concat = malloc(sizeof(char) * (len + 1));
 
-    if (concatenated == NULL)
-        return (NULL);
+	if (concat == NULL)
+		return (NULL);
 
-    total_length = 0;
+	len = 0;
 
-    for (i = 0; s1[i]; i++)
-        concatenated[total_length++] = s1[i];
+	for (index = 0; s1[index]; index++)
+		concat[len++] = s1[index];
 
-    for (i = 0; s2[i] && i < n; i++)
-        concatenated[total_length++] = s2[i];
+	for (index = 0; s2[index] && index < n; index++)
+		concat[len++] = s2[index];
 
-    concatenated[total_length] = '\0';
+	concat[len] = '\0';
 
-    return (concatenated);
+	return (concat);
 }
