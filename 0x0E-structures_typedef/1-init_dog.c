7978f8b6c925a65bd8b9cd3feb276c69a439ff39
zoz01 <ziadgad2002@gmail.com>
2023-09-12 00:52:34 +0300

    m

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,4 @@
-#ifndef DOG_H
-#define DOG_H
+#include "dog.h"
 
 /**
 * struct dog - a sent said by a dog
@@ -10,13 +9,11 @@
 * Description: it will say oofffff as a dog
 */
 
-struct dog
-{
-char *name;
-float age;
-char *owner;
-};
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
-#endif
+{
+if (d){
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
+}
