b7ac3a23fc1d20d12b0875ba4383a688ec7417a6
zoz01 <ziadgad2002@gmail.com>
2023-09-12 00:55:16 +0300

    m

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,11 +9,12 @@
 * Description: it will say oofffff as a dog
 */
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-if (d){
-d->name = name;
-d->age = age;
-d->owner = owner;
-}
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
