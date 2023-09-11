20602596d18b20d399977b8c7cab1bfed7cfff0f
zoz01 <ziadgad2002@gmail.com>
2023-09-11 11:11:05 -0700

    My first commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,18 +2,15 @@
 #define DOG_H
 
 /**
-* main - check the code
 * struct dog - a sent said by a dog
 * @name: the dog name
-* @age: Sthde age
+* @age: the dog age
 * @owner: the name of the owner
 * Description: it will say oofffff as a dog
-* Return: Always 0.
 */
 
 typedef struct dog
 {
-    /* ooooooof */
     char *name ;
     float age  ;
     char *owner;
