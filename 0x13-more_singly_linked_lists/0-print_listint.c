ad7361d7dff7a24dc291dad58597bedeb689e249
zoz01 <ziadgad2002@gmail.com>
2023-09-26 12:56:03 +0300

    00

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,19 +1,27 @@
 #include "lists.h"
-
 /**
- * print_listint - function to print all elements of list
- * @h: pointer to the elements of lists
- * Return: count of the nodes
- */
-size_t print_listint(const listint_t *h)
+* print_list - the poin dui
+* @h: dfh dlu
+* Return: doifpo sioufm idf iff
+*/
+size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+size_t n = 0;
 
-	while (h != NULL)
-	{
-		printf("%d\n", h->n); /* alternative way to write - (*h).n */
-		h = h->next; /* alternative wat to write (*h).next */
-		count++;
-	}
-	return (count);
+while (h != NULL)
+{
+if (h->str == NULL)
+{
+printf("[0] %p\n", h->str);
+h = h->next;
+n++;
+}
+else
+{
+printf("[%d] %s\n", h->len, h->str);
+h = h->next;
+n++;
+}
+}
+return (n);
 }
